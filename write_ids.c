4867551d89884ba5f12836d34ec7572678401024
TylerDows <tmd6040@psu.edu>
2024-09-10 15:15:08 -0400

    Your message

--- write_ids.c
+++ write_ids.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    char GITHUB_ID[] = "Fill_in_Your_Github_Id";
-    char STUDENT_ID[] = "Fill_in_Your_Student_Id";
+    char GITHUB_ID[] = "TylerDows";
+    char STUDENT_ID[] = "908926204";
 
     return 0 ;
 }
